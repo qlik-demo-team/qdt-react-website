760234675049cdd5c7678012962cc426bf4106b9
Arturo Munoz <AMZ@qlik.com>
2019-11-19 08:18:20 -0500

    new banner React and Qdt

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => (
   <div>
     <div className="row">
       <div className="col-md-12 text-center">
-        <a href="#/"><img src="https://cdn.rawgit.com/qlik-demo-team/qdt-react-template/223fe135/src/assets/banner_react.jpg" className="banner img-fluid" alt="banner" /></a>
+        <a href="#/"><img src="https://cdn.rawgit.com/qlik-demo-team/qdt-react-template/223fe135/src/assets/react_Qdt.png" className="banner img-fluid" alt="Qdt and React" /></a>
         <h3>
             This is a React (
           {dependencies.react}
